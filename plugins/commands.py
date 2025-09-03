333c69aef8c2ed42c0076559053d11e37d8b0dbf
jnaneshgowda123 <104252761+jnaneshgowda123@users.noreply.github.com>
2025-09-03 22:34:54 +0600

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -260,7 +260,7 @@ async def start(client, message):
                         InlineKeyboardButton("♻️ ᴛʀʏ ᴀɢᴀɪɴ ♻️", callback_data=f"checksub#{kk}#{file_id}")
                     ])
                     reply_markup = InlineKeyboardMarkup(btn)
-                photo = random.choice(FSUB_PICS) if FSUB_PICS else "https://graph.org/file/7478ff3eac37f4329c3d8.jpg"
+                photo = random.choice(FSUB_PICS) if FSUB_PICS else "https://i.ibb.co/XfN1dKDP/x.jpg"
                 caption = (
                     f"👋 ʜᴇʟʟᴏ {message.from_user.mention}\n\n"
                     "🛑 ʏᴏᴜ ᴍᴜsᴛ ᴊᴏɪɴ ᴛʜᴇ ʀᴇǫᴜɪʀᴇᴅ ᴄʜᴀɴɴᴇʟs ᴛᴏ ᴄᴏɴᴛɪɴᴜᴇ.\n"
