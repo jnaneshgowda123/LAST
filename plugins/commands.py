4507563da28d0a5e776b16754c60451391b6ed7e
jnaneshgowda123 <104252761+jnaneshgowda123@users.noreply.github.com>
2025-09-04 21:49:41 +0600

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -97,15 +97,13 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
-        buttons = [[
-                    InlineKeyboardButton('🔰 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🔰', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
-                ],[
-                    InlineKeyboardButton(' ʜᴇʟᴘ 📢', callback_data='help'),
-                    InlineKeyboardButton(' ᴀʙᴏᴜᴛ 📖', callback_data='about')
-                ],[
-                    InlineKeyboardButton('ᴛᴏᴘ sᴇᴀʀᴄʜɪɴɢ ⭐', callback_data="topsearch"),
-                    InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ 🎟', callback_data="premium_info"),
-                ]]
+        buttons = [
+                        [InlineKeyboardButton('🛎 MAIN CHANNEL 🛎', url=UPDATE_CHNL_LNK)],
+                        [InlineKeyboardButton('🖥️ NEW RELEASED MOVIES 🖥️', url='https://t.me/+Fi9MNuaisWwxZDI1')],
+                        [InlineKeyboardButton('📫 OTT RELEASED MOVIES 📫', url='https://t.me/+rJUcsBEWwYg3YzI1'), 
+                         InlineKeyboardButton('📫 OTT RELEASED KANNADA MOVIES 📫', url='https://t.me/+NWXPZGgS1zQ3YWE1')],
+                        [InlineKeyboardButton('🔥 ADULT CHANNEL 🔥', url='https://t.me/+01z_dRj5wmgyNWE1')]
+                    ]
         reply_markup = InlineKeyboardMarkup(buttons)
         current_time = datetime.now(pytz.timezone(TIMEZONE))
         curr_time = current_time.hour        
@@ -129,15 +127,13 @@ async def start(client, message):
         return
 
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
-        buttons = [[
-                    InlineKeyboardButton('🔰 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🔰', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
-                ],[
-                    InlineKeyboardButton(' ʜᴇʟᴘ 📢', callback_data='help'),
-                    InlineKeyboardButton(' ᴀʙᴏᴜᴛ 📖', callback_data='about')
-                ],[
-                    InlineKeyboardButton('ᴛᴏᴘ sᴇᴀʀᴄʜɪɴɢ ⭐', callback_data="topsearch"),
-                    InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ 🎟', callback_data="premium_info"),
-                ]]
+        buttons = [
+                        [InlineKeyboardButton('🛎 MAIN CHANNEL 🛎', url=UPDATE_CHNL_LNK)],
+                        [InlineKeyboardButton('🖥️ NEW RELEASED MOVIES 🖥️', url='https://t.me/+Fi9MNuaisWwxZDI1')],
+                        [InlineKeyboardButton('📫 OTT RELEASED MOVIES 📫', url='https://t.me/+rJUcsBEWwYg3YzI1'), 
+                         InlineKeyboardButton('📫 OTT RELEASED KANNADA MOVIES 📫', url='https://t.me/+NWXPZGgS1zQ3YWE1')],
+                        [InlineKeyboardButton('🔥 ADULT CHANNEL 🔥', url='https://t.me/+01z_dRj5wmgyNWE1')]
+                    ]
         reply_markup = InlineKeyboardMarkup(buttons)
         current_time = datetime.now(pytz.timezone(TIMEZONE))
         curr_time = current_time.hour        
@@ -369,9 +365,11 @@ async def start(client, message):
                         ]
                 else:
                     btn = [
-                        [InlineKeyboardButton('📌 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ 📌', url=UPDATE_CHNL_LNK)],
+                        [InlineKeyboardButton('🛎 MAIN CHANNEL 🛎', url=UPDATE_CHNL_LNK)],
                         [InlineKeyboardButton('🖥️ NEW RELEASED MOVIES 🖥️', url='https://t.me/+Fi9MNuaisWwxZDI1')],
-                        [InlineKeyboardButton('🔥 adult bot 🔥', url='https://t.me/newxxxiobot?start=restart')]
+                        [InlineKeyboardButton('📫 OTT RELEASED MOVIES 📫', url='https://t.me/+rJUcsBEWwYg3YzI1'), 
+                         InlineKeyboardButton('📫 OTT RELEASED KANNADA MOVIES 📫', url='https://t.me/+NWXPZGgS1zQ3YWE1')],
+                        [InlineKeyboardButton('🔥 ADULT CHANNEL 🔥', url='https://t.me/+01z_dRj5wmgyNWE1')]
                     ]
                 msg = await client.send_cached_media(
                     chat_id=message.from_user.id,
@@ -381,11 +379,11 @@ async def start(client, message):
                     reply_markup=InlineKeyboardMarkup(btn)
                 )
                 filesarr.append(msg)
-            k = await client.send_message(chat_id=message.from_user.id, text=f"<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nᴛʜɪꜱ ᴍᴏᴠɪᴇ ꜰɪʟᴇ/ᴠɪᴅᴇᴏ ᴡɪʟʟ ʙᴇ ᴅᴇʟᴇᴛᴇᴅ ɪɴ <b><u><code>{get_time(DELETE_TIME)}</code></u> 🫥 <i></b>(ᴅᴜᴇ ᴛᴏ ᴄᴏᴘʏʀɪɢʜᴛ ɪꜱꜱᴜᴇꜱ)</i>.\n\n<b><i>ᴘʟᴇᴀꜱᴇ ꜰᴏʀᴡᴀʀᴅ ᴛʜɪꜱ ꜰɪʟᴇ ᴛᴏ ꜱᴏᴍᴇᴡʜᴇʀᴇ ᴇʟꜱᴇ ᴀɴᴅ ꜱᴛᴀʀᴛ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴛʜᴇʀᴇ</i></b>")
+            k = await client.send_message(chat_id=message.from_user.id, text=f"<strong>📤 This File Will Be Delete In : <u><code>{get_time(DELETE_TIME)}</code></u>⏰\n\n<u>Join This Group And Ask Any Movies</u>\n\nGroup 1\nhttps://t.me/+dd9gZo9nlg9mNjQ1\nGroup 2\nhttps://t.me/+dau0zdsJPhI2OWNl</strong>")
             await asyncio.sleep(DELETE_TIME)
             for x in filesarr:
                 await x.delete()
-            await k.edit_text("<b>ʏᴏᴜʀ ᴀʟʟ ᴠɪᴅᴇᴏꜱ/ꜰɪʟᴇꜱ ᴀʀᴇ ᴅᴇʟᴇᴛᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ !\nᴋɪɴᴅʟʏ ꜱᴇᴀʀᴄʜ ᴀɢᴀɪɴ</b>")
+            await k.edit_text("<b>ʏᴏᴜʀ ᴠɪᴅᴇᴏ / ꜰɪʟᴇ ɪꜱ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ᴅᴇʟᴇᴛᴇᴅ !!\n\n<u>Join This Group And Ask Any Movies</u>\n\nGroup 1\nhttps://t.me/+dd9gZo9nlg9mNjQ1\nGroup 2\nhttps://t.me/+dau0zdsJPhI2OWNl</b>")
             return
         except Exception as e:
             logger.exception(e)
@@ -416,10 +414,12 @@ async def start(client, message):
             else:
             
                 btn = [
-                    [InlineKeyboardButton('📌 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ 📌', url=UPDATE_CHNL_LNK)],
-                    [InlineKeyboardButton('🖥️ NEW RELEASED MOVIES 🖥️', url='https://t.me/+Fi9MNuaisWwxZDI1')],
-                    [InlineKeyboardButton('🔥 adult bot 🔥', url='https://t.me/newxxxiobot?start=restart')]
-               ] 
+                        [InlineKeyboardButton('🛎 MAIN CHANNEL 🛎', url=UPDATE_CHNL_LNK)],
+                        [InlineKeyboardButton('🖥️ NEW RELEASED MOVIES 🖥️', url='https://t.me/+Fi9MNuaisWwxZDI1')],
+                        [InlineKeyboardButton('📫 OTT RELEASED MOVIES 📫', url='https://t.me/+rJUcsBEWwYg3YzI1'), 
+                         InlineKeyboardButton('📫 OTT RELEASED KANNADA MOVIES 📫', url='https://t.me/+NWXPZGgS1zQ3YWE1')],
+                        [InlineKeyboardButton('🔥 ADULT CHANNEL 🔥', url='https://t.me/+01z_dRj5wmgyNWE1')]
+                    ] 
             msg = await client.send_cached_media(
                 chat_id=message.from_user.id,
                 file_id=file_id,
@@ -443,15 +443,12 @@ async def start(client, message):
                 reply_markup=InlineKeyboardMarkup(btn)
             )
             k = await msg.reply(
-                f"<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\n"
-                f"ᴛʜɪꜱ ᴍᴏᴠɪᴇ ꜰɪʟᴇ/ᴠɪᴅᴇᴏ ᴡɪʟʟ ʙᴇ ᴅᴇʟᴇᴛᴇᴅ ɪɴ <b><u><code>{get_time(DELETE_TIME)}</code></u> 🫥 <i></b>"
-                "(ᴅᴜᴇ ᴛᴏ ᴄᴏᴘʏʀɪɢʜᴛ ɪꜱꜱᴜᴇꜱ)</i>.\n\n"
-                "<b><i>ᴘʟᴇᴀꜱᴇ ꜰᴏʀᴡᴀʀᴅ ᴛʜɪꜱ ꜰɪʟᴇ ᴛᴏ ꜱᴏᴍᴇᴡʜᴇʀᴇ ᴇʟꜱᴇ ᴀɴᴅ ꜱᴛᴀʀᴛ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴛʜᴇʀᴇ</i></b>",
+                f"<strong>📤 This File Will Be Delete In : <u><code>{get_time(DELETE_TIME)}</code></u>⏰\n\n<u>Join This Group And Ask Any Movies</u>\n\nGroup 1\nhttps://t.me/+dd9gZo9nlg9mNjQ1\nGroup 2\nhttps://t.me/+dau0zdsJPhI2OWNl</strong>",
                 quote=True
             )
             await asyncio.sleep(DELETE_TIME)
             await msg.delete()
-            await k.edit_text("<b>ʏᴏᴜʀ ᴠɪᴅᴇᴏ / ꜰɪʟᴇ ɪꜱ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ᴅᴇʟᴇᴛᴇᴅ !!</b>")
+            await k.edit_text("<b>ʏᴏᴜʀ ᴠɪᴅᴇᴏ / ꜰɪʟᴇ ɪꜱ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ᴅᴇʟᴇᴛᴇᴅ !!\n\n<u>Join This Group And Ask Any Movies</u>\n\nGroup 1\nhttps://t.me/+dd9gZo9nlg9mNjQ1\nGroup 2\nhttps://t.me/+dau0zdsJPhI2OWNl</b>")
             return
         except Exception as e:
             logger.exception(e)
@@ -492,10 +489,12 @@ async def start(client, message):
             ]
     else:
         btn = [
-                    [InlineKeyboardButton('📌 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ 📌', url=UPDATE_CHNL_LNK)],
-                    [InlineKeyboardButton('🖥️ NEW RELEASED MOVIES 🖥️', url='https://t.me/+Fi9MNuaisWwxZDI1')],
-                    [InlineKeyboardButton('🔥 adult bot 🔥', url='https://t.me/newxxxiobot?start=restart')]
-               ]
+                        [InlineKeyboardButton('🛎 MAIN CHANNEL 🛎', url=UPDATE_CHNL_LNK)],
+                        [InlineKeyboardButton('🖥️ NEW RELEASED MOVIES 🖥️', url='https://t.me/+Fi9MNuaisWwxZDI1')],
+                        [InlineKeyboardButton('📫 OTT RELEASED MOVIES 📫', url='https://t.me/+rJUcsBEWwYg3YzI1'), 
+                         InlineKeyboardButton('📫 OTT RELEASED KANNADA MOVIES 📫', url='https://t.me/+NWXPZGgS1zQ3YWE1')],
+                        [InlineKeyboardButton('🔥 ADULT CHANNEL 🔥', url='https://t.me/+01z_dRj5wmgyNWE1')]
+                    ]
     msg = await client.send_cached_media(
         chat_id=message.from_user.id,
         file_id=file_id,
@@ -504,15 +503,12 @@ async def start(client, message):
         reply_markup=InlineKeyboardMarkup(btn)
     )
     k = await msg.reply(
-        f"<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\n"
-        f"ᴛʜɪꜱ ᴍᴏᴠɪᴇ ꜰɪʟᴇ/ᴠɪᴅᴇᴏ ᴡɪʟʟ ʙᴇ ᴅᴇʟᴇᴛᴇᴅ ɪɴ <b><u><code>{get_time(DELETE_TIME)}</code></u> 🫥 <i></b>"
-        "(ᴅᴜᴇ ᴛᴏ ᴄᴏᴘʏʀɪɢʜᴛ ɪꜱꜱᴜᴇꜱ)</i>.\n\n"
-        "<b><i>ᴘʟᴇᴀꜱᴇ ꜰᴏʀᴡᴀʀᴅ ᴛʜɪꜱ ꜰɪʟᴇ ᴛᴏ ꜱᴏᴍᴇᴡʜᴇʀᴇ ᴇʟꜱᴇ ᴀɴᴅ ꜱᴛᴀʀᴛ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴛʜᴇʀᴇ</i></b>",
+        f"<strong>📤 This File Will Be Delete In : <u><code>{get_time(DELETE_TIME)}</code></u>⏰\n\n<u>Join This Group And Ask Any Movies</u>\n\nGroup 1\nhttps://t.me/+dd9gZo9nlg9mNjQ1\nGroup 2\nhttps://t.me/+dau0zdsJPhI2OWNl</strong>",
         quote=True
     )     
     await asyncio.sleep(DELETE_TIME)
     await msg.delete()
-    await k.edit_text("<b>ʏᴏᴜʀ ᴠɪᴅᴇᴏ / ꜰɪʟᴇ ɪꜱ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ᴅᴇʟᴇᴛᴇᴅ !!</b>")
+    await k.edit_text("<b>ʏᴏᴜʀ ᴠɪᴅᴇᴏ / ꜰɪʟᴇ ɪꜱ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ᴅᴇʟᴇᴛᴇᴅ !!\n\n<u>Join This Group And Ask Any Movies</u>\n\nGroup 1\nhttps://t.me/+dd9gZo9nlg9mNjQ1\nGroup 2\nhttps://t.me/+dau0zdsJPhI2OWNl</b>")
     return
 
 @Client.on_message(filters.command('logs') & filters.user(ADMINS))
